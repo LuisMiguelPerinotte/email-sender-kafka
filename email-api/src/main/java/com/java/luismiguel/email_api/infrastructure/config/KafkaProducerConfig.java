2e6f28eef9b2d1d382153b84cffb27e3f7b64600
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-05-25 14:43:14 -0300

    feat: add KafkaProducer configuration class

---
+++ email-api/src/main/java/com/java/luismiguel/email_api/infrastructure/config/KafkaProducerConfig.java
@@ -0,0 +1,38 @@
+package com.java.luismiguel.email_api.infrastructure.config;
+
+import com.java.luismiguel.email_api.infrastructure.kafka.event.EmailSendRequestedEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAccess;
+
+    @Bean
+    public ProducerFactory<String, EmailSendRequestedEvent> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAccess);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, EmailSendRequestedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
