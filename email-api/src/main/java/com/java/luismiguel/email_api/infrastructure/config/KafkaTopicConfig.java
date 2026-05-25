9c6d875ed6d358affa503128e797b70b657802a2
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-05-25 14:43:29 -0300

    feat: add KafkaTopic configuration class

---
+++ email-api/src/main/java/com/java/luismiguel/email_api/infrastructure/config/KafkaTopicConfig.java
@@ -0,0 +1,30 @@
+package com.java.luismiguel.email_api.infrastructure.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAccess;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAccess);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topicEmailRequest() {
+        return new NewTopic("email.send.requested", 2, (short) 1);
+    }
+}
