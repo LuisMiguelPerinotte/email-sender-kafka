cec7f0ae8151c70397be75a939217996a7c89855
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-05-28 11:20:53 -0300

    feat: adapting service for retry implementation

--- email-worker/src/main/java/com/java/luismiguel/email_worker/application/email/EmailSendingService.java
+++ email-worker/src/main/java/com/java/luismiguel/email_worker/application/email/EmailSendingService.java
@@ -50,7 +50,10 @@ public class EmailSendingService {
         } catch (MailException e) {
             emailRequest.setStatus(EmailRequestStatus.FAILED);
             emailRequest.setErrorMessage(e.getMessage());
+            emailRequestRepository.save(emailRequest);
+
             log.error("error sending E-mail. E-mail Request Id= {}", emailRequestId);
+            throw e;
         }
 
         emailRequestRepository.save(emailRequest);
