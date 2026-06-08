f5bb6f4da2a4ea3e320b73ee4769ba6f96027a07
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-06-08 13:14:36 -0300

    test: add integration tests for email retry functionality in EmailService

--- email-api/src/test/java/com/java/luismiguel/email_api/application/service/email/EmailServiceIntegrationTest.java
+++ email-api/src/test/java/com/java/luismiguel/email_api/application/service/email/EmailServiceIntegrationTest.java
@@ -6,7 +6,9 @@ import com.java.luismiguel.email_api.api.dto.email.response.SendEmailResponseDTO
 import com.java.luismiguel.email_api.domain.email.EmailRequest;
 import com.java.luismiguel.email_api.domain.email.EmailRequestRepository;
 import com.java.luismiguel.email_api.domain.email.enums.EmailRequestStatus;
+import com.java.luismiguel.email_api.infrastructure.exception.business.email.EmailCannotBeRetryableException;
 import com.java.luismiguel.email_api.infrastructure.exception.business.email.EmailEventPublishException;
+import com.java.luismiguel.email_api.infrastructure.exception.business.email.EmailRequestNotFoundException;
 import com.java.luismiguel.email_api.infrastructure.kafka.producer.EmailEventProducer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -118,6 +120,168 @@ public class EmailServiceIntegrationTest {
 
             assertThat(savedEmailRequest.getStatus()).isEqualTo(EmailRequestStatus.PUBLISH_FAILED);
             assertThat(savedEmailRequest.getErrorMessage()).isEqualTo("Kafka publish failed");
+        }
+    }
+
+
+    @Nested
+    @DisplayName("retryEmail")
+    class RetryEmail {
+
+        @BeforeEach
+        void setUp() {
+            emailRequestRepository.deleteAll();
+        }
+
+        @Test
+        @DisplayName("Should Retry Failed Email Successfully")
+        void shouldRetryFailedEmailSuccessfully() {
+            // given
+            EmailRequest failedEmailRequest =
+                    EmailRequest.builder()
+                            .recipient("recipient")
+                            .subject("subject")
+                            .body("body")
+                            .attempts(4)
+                            .errorMessage("SMTP error")
+                            .status(EmailRequestStatus.FAILED)
+                            .build();
+
+            EmailRequest savedFailedEmailRequest = emailRequestRepository.save(failedEmailRequest);
+
+            // when
+            RetryEmailSendResponseDTO responseDTO =
+                    emailService.retryEmail(savedFailedEmailRequest.getEmailRequestId());
+
+            // then
+            assertThat(responseDTO.emailId()).isNotNull();
+            assertThat(responseDTO.status()).isEqualTo(EmailRequestStatus.PENDING);
+
+            Optional<EmailRequest> optionalEmailRequest =
+                    emailRequestRepository.findById(responseDTO.emailId());
+
+            assertThat(optionalEmailRequest).isPresent();
+
+            EmailRequest savedEmailRequest = optionalEmailRequest.get();
+
+            assertThat(savedEmailRequest.getErrorMessage()).isNull();
+            assertThat(savedEmailRequest.getStatus()).isEqualTo(EmailRequestStatus.PENDING);
+
+            then(emailEventProducer).should()
+                    .publishEmailSendRequested(responseDTO.emailId());
+        }
+
+
+        @Test
+        @DisplayName("Should Retry Publish Failed Email Successfully")
+        void shouldRetryPublishFailedEmailSuccessfully() {
+            // given
+            EmailRequest publishFailedEmailRequest =
+                    EmailRequest.builder()
+                            .recipient("recipient")
+                            .subject("subject")
+                            .body("body")
+                            .attempts(4)
+                            .errorMessage("publish failed")
+                            .status(EmailRequestStatus.PUBLISH_FAILED)
+                            .build();
+
+            EmailRequest savedFailedEmailRequest = emailRequestRepository.save(publishFailedEmailRequest);
+
+            // when
+            RetryEmailSendResponseDTO responseDTO =
+                    emailService.retryEmail(savedFailedEmailRequest.getEmailRequestId());
+
+            // then
+            assertThat(responseDTO.emailId()).isNotNull();
+            assertThat(responseDTO.status()).isEqualTo(EmailRequestStatus.PENDING);
+
+            Optional<EmailRequest> optionalEmailRequest =
+                    emailRequestRepository.findById(responseDTO.emailId());
+
+            assertThat(optionalEmailRequest).isPresent();
+
+            EmailRequest savedEmailRequest = optionalEmailRequest.get();
+
+            assertThat(savedEmailRequest.getErrorMessage()).isNull();
+
+            then(emailEventProducer).should()
+                    .publishEmailSendRequested(responseDTO.emailId());
+        }
+
+
+        @Test
+        @DisplayName("Should Throw Exception When Email Status Is Not Retryable")
+        void shouldThrowExceptionWhenEmailStatusIsNotRetryable() {
+            // given
+            EmailRequest emailRequest =
+                    EmailRequest.builder()
+                            .recipient("recipient")
+                            .subject("subject")
+                            .body("body")
+                            .attempts(1)
+                            .status(EmailRequestStatus.SENT)
+                            .build();
+
+            EmailRequest savedEmailRequest = emailRequestRepository.save(emailRequest);
+
+            // when + then
+            assertThrows(EmailCannotBeRetryableException.class, () -> {
+                emailService.retryEmail(savedEmailRequest.getEmailRequestId());
+            });
+
+            then(emailEventProducer).shouldHaveNoInteractions();
+        }
+
+
+        @Test
+        @DisplayName("Should Throw Exception When Email Request Does Not Exist")
+        void shouldThrowExceptionWhenEmailRequestDoesNotExist() {
+            // when + then
+            assertThrows(EmailRequestNotFoundException.class, () -> {
+                emailService.retryEmail(UUID.randomUUID());
+            });
+        }
+
+
+        @Test
+        @DisplayName("Should Set Publish Failed When Retry Publishing Fails")
+        void shouldSetPublishFailedWhenRetryPublishingFails() {
+            // given
+            EmailRequest emailRequest = EmailRequest.builder()
+                    .recipient("user@email.com")
+                    .subject("Hello")
+                    .body("Testing Kafka")
+                    .attempts(1)
+                    .status(EmailRequestStatus.FAILED)
+                    .errorMessage("SMTP error")
+                    .build();
+
+            EmailRequest savedEmail = emailRequestRepository.save(emailRequest);
+
+            RuntimeException cause = new RuntimeException("Kafka unavailable");
+
+            willThrow(new EmailEventPublishException("Kafka publish failed", cause))
+                    .given(emailEventProducer)
+                    .publishEmailSendRequested(savedEmail.getEmailRequestId());
+
+            // when + then
+            assertThrows(EmailEventPublishException.class, () -> {
+                emailService.retryEmail(savedEmail.getEmailRequestId());
+            });
+
+            Optional<EmailRequest> optionalEmailRequest =
+                    emailRequestRepository.findById(savedEmail.getEmailRequestId());
+
+            assertThat(optionalEmailRequest).isPresent();
+
+            EmailRequest updatedEmail = optionalEmailRequest.get();
+
+            assertThat(updatedEmail.getStatus()).isEqualTo(EmailRequestStatus.PUBLISH_FAILED);
+            assertThat(updatedEmail.getErrorMessage()).isEqualTo("Kafka publish failed");
+
+            then(emailEventProducer).should()
+                    .publishEmailSendRequested(savedEmail.getEmailRequestId());
         }
     }
 }
