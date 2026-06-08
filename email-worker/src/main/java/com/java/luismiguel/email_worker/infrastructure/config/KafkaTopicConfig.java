ff341308e61be7ad3a8ca8d7b9e2d4e0c13a7cc6
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-06-08 14:22:55 -0300

    chore: add profile annotation to Kafka configuration classes for test exclusion

--- email-worker/src/main/java/com/java/luismiguel/email_worker/infrastructure/config/KafkaTopicConfig.java
+++ email-worker/src/main/java/com/java/luismiguel/email_worker/infrastructure/config/KafkaTopicConfig.java
@@ -3,8 +3,10 @@ package com.java.luismiguel.email_worker.infrastructure.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class KafkaTopicConfig {
 
     @Bean
